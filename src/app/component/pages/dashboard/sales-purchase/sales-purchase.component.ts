dca137c4acdf40531638e70ed62a87eec741ef31
Arunagiri <arunagiri1706@gmail.com>
2024-02-03 12:15:02 +0530

    Cards updated

--- src/app/component/pages/dashboard/sales-purchase/sales-purchase.component.ts
+++ src/app/component/pages/dashboard/sales-purchase/sales-purchase.component.ts
@@ -8,6 +8,51 @@ import { commonData } from '../../../../shared/data/component/widget/general/gen
 })
 export class SalesPurchaseComponent {
 
-  public commonData = commonData;
+  public commonData =  [
+    {
+        icon: 'cart',
+        values: '17',
+        title: 'No. of Variety',
+        color: 'secondary'
+    },
+    {
+        icon: 'Home',
+        values: '3053 / 1050.96',
+        title: 'Opening Stock',
+        color: ' success',
+        
+    },
+    {
+        icon: 'rate',
+        values: '1471 / 539.86',
+        title: 'Receipt',
+        color: ' warning',
+        
+    },
+    {
+        icon: 'return-box',
+        values: '0 / 0',
+        title: 'SR-Return',
+        color: 'primary'
+    },
+    {
+        icon: 'arrowright',
+        values: '2147 / 782.81',
+        title: 'Issue',
+        color: 'secondary'
+    },
+    {
+        icon: 'rate',
+        values: '0 / 0',
+        title: 'Issue Return',
+        color: 'warning'
+    },
+    {
+        icon: 'new-order',
+        values: '2377 / 808.31',
+        title: 'Closing Stock',
+        color: 'success'
+    },
+];
 
 }
