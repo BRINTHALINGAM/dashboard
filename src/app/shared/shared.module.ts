3491cdef3ceea7b121832f9895c6eff0c4a8f2ae
Gowreesh E A <gowreeshea@gmail.com>
2024-02-07 09:43:55 +0530

    updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -17,21 +17,22 @@ import { CartComponent } from "./component/header/cart/cart.component";
 import { ThemeComponent } from "./component/header/theme/theme.component";
 import { LanguageComponent } from "./component/header/language/language.component";
 import { ProfileComponent } from "./component/header/profile/profile.component";
-import { SvgIconComponent } from "./component/svg-icon/svg-icon.component";
+import { SvgIconComponent } from "./component/svg-icon/svg-icon.component"; 
 import { BreadcrumbComponent } from "./component/breadcrumb/breadcrumb.component";
 import { SearchComponent } from "./component/header/search/search.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TapToTopComponent } from "./component/tap-to-top/tap-to-top.component";
 import { LoaderComponent } from "./component/loader/loader.component";
-import { DashmenuComponent } from "./component/header/dashmenu/dashmenu.component";
+import { DashmenuComponent } from "./component/header/dashmenu/dashmenu.component";  
+import { DateComponent } from "./component/header/date/date.component";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
 @NgModule({
-  declarations: [ContentComponent, HeaderComponent, SidebarComponent, FeathericonComponent, FooterComponent, NotificationComponent, BookmarkComponent, MessageComponent, CartComponent, ThemeComponent, LanguageComponent, ProfileComponent, SvgIconComponent, BreadcrumbComponent, SearchComponent, TapToTopComponent, LoaderComponent, DashmenuComponent],
+  declarations: [ContentComponent, HeaderComponent, SidebarComponent, FeathericonComponent, FooterComponent, NotificationComponent, BookmarkComponent, MessageComponent, CartComponent, ThemeComponent, LanguageComponent, ProfileComponent, SvgIconComponent, BreadcrumbComponent, SearchComponent, TapToTopComponent, LoaderComponent, DashmenuComponent,DateComponent     ],
   imports: [CommonModule, SharedRoutingModule, NgbModule, ReactiveFormsModule, FormsModule, AngularSvgIconModule.forRoot(), TranslateModule.forRoot()],
   exports: [ContentComponent, FeathericonComponent, LoaderComponent, SvgIconComponent, BreadcrumbComponent, TapToTopComponent, TranslateModule, NgbModule],
 })
