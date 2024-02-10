94090d33ee3fdedb85a66943ab0456e935a5fd5d
Gowreesh E A <gowreeshea@gmail.com>
2024-02-10 09:50:46 +0530

    Removed Unwanted Files

--- src/app/component/pages/pages-routing.module.ts
+++ src/app/component/pages/pages-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
-import { SamplePage1Component } from "./sample-page1/sample-page1.component";
-import { SamplePage2Component } from "./sample-page2/sample-page2.component";
+
 import { RouterModule, Routes } from "@angular/router";
 import { RMIDashboardComponent } from "./dashboard/rmi-dashboard.component";
 
@@ -16,14 +15,7 @@ const routes: Routes = [
           breadcrumb: "Default",
         },
       },
-      {
-        path: "sample-page2",
-        component: SamplePage2Component,
-        data: {
-          title: "Sample-page2",
-          breadcrumb: "Sample-page2",
-        },
-      },
+      
     ],
   },
 ];
--- src/app/component/pages/pages.module.ts
+++ src/app/component/pages/pages.module.ts
@@ -5,13 +5,12 @@ import { barChart, columnChart2 } from './../../shared/data/component/charts/cha
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PagesRoutingModule } from "./pages-routing.module";
-import { SamplePage1Component } from "./sample-page1/sample-page1.component";
-import { SamplePage2Component } from "./sample-page2/sample-page2.component";
+
 import { RMIDashboardComponent } from "./dashboard/rmi-dashboard.component";
 import { NgApexchartsModule } from "ng-apexcharts";
 import { SalesSummaryComponent } from "./dashboard/sales-summary/sales-summary.component";
 import { CommonAnalyicsComponent } from "./dashboard/common-analyics/common-analyics.component";
-import { CoursesComponent } from "./dashboard/courses/courses.component";
+
 import { SalesPurchaseComponent } from "./dashboard/sales-purchase/sales-purchase.component";
 import { ChartistModule } from "ng-chartist";
 import {Ng2GoogleChartsModule} from "Ng2-google-charts";
@@ -23,7 +22,7 @@ import { ConsumptionComponent } from './dashboard/consumption/consumption.compon
 import {NgxGaugeModule} from "ngx-gauge";
 
 @NgModule({
-  declarations: [RMIDashboardComponent, SalesSummaryComponent, CommonAnalyicsComponent, SalesPurchaseComponent, CoursesComponent, 
+  declarations: [RMIDashboardComponent, SalesSummaryComponent, CommonAnalyicsComponent, SalesPurchaseComponent, 
     SalesSummary1Component,RmreceiptComponent,MixwiseComponent,TopsuppliersComponent,ValuesComponent,ConsumptionComponent,
   ],
   imports: [CommonModule, PagesRoutingModule, NgApexchartsModule,Ng2GoogleChartsModule, NgxGaugeModule],
