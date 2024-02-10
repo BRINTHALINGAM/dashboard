d637955fd133f8c3e04973027409edbf9c15aac3
Gowreesh E A <gowreeshea@gmail.com>
2024-02-10 13:36:39 +0530

    Removed Unused Files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,19 +7,11 @@ import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { HttpClient } from "@angular/common/http";
 import { ContentComponent } from "./component/layout/content/content.component";
 import { HeaderComponent } from "./component/header/header.component";
-import { SidebarComponent } from "./component/sidebar/sidebar.component";
 import { FeathericonComponent } from "./component/feathericon/feathericon.component";
 import { FooterComponent } from "./component/footer/footer.component";
-import { NotificationComponent } from "./component/header/notification/notification.component";
-import { BookmarkComponent } from "./component/header/bookmark/bookmark.component";
-import { MessageComponent } from "./component/header/message/message.component";
-import { CartComponent } from "./component/header/cart/cart.component";
 import { ThemeComponent } from "./component/header/theme/theme.component";
-import { LanguageComponent } from "./component/header/language/language.component";
 import { ProfileComponent } from "./component/header/profile/profile.component";
 import { SvgIconComponent } from "./component/svg-icon/svg-icon.component"; 
-import { BreadcrumbComponent } from "./component/breadcrumb/breadcrumb.component";
-import { SearchComponent } from "./component/header/search/search.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TapToTopComponent } from "./component/tap-to-top/tap-to-top.component";
@@ -32,8 +24,10 @@ export function HttpLoaderFactory(http: HttpClient) {
 }
 
 @NgModule({
-  declarations: [ContentComponent, HeaderComponent, SidebarComponent, FeathericonComponent, FooterComponent, NotificationComponent, BookmarkComponent, MessageComponent, CartComponent, ThemeComponent, LanguageComponent, ProfileComponent, SvgIconComponent, BreadcrumbComponent, SearchComponent, TapToTopComponent, LoaderComponent, DashmenuComponent,DateComponent     ],
+  declarations: [ContentComponent, HeaderComponent, FeathericonComponent, FooterComponent, ThemeComponent,  ProfileComponent, SvgIconComponent,  
+  TapToTopComponent, LoaderComponent, DashmenuComponent,DateComponent     ],
   imports: [CommonModule, SharedRoutingModule, NgbModule, ReactiveFormsModule, FormsModule, AngularSvgIconModule.forRoot(), TranslateModule.forRoot()],
-  exports: [ContentComponent, FeathericonComponent, LoaderComponent, SvgIconComponent, BreadcrumbComponent, TapToTopComponent, TranslateModule, NgbModule],
+  exports: [ContentComponent, FeathericonComponent, LoaderComponent, SvgIconComponent,  
+  TapToTopComponent, TranslateModule, NgbModule],
 })
 export class SharedModule {}
