8d68f207078522af9de84a0244a2657250ce41ce
BRINTHA1102 <brinthalingam@gmail.com>
2024-02-16 09:45:06 +0530

    card value rounded off

--- src/app/services/posthttp.service.ts
+++ src/app/services/posthttp.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class PosthttpService {
 
-  private baseUrl="http://172.16.16.52:8100/api/YarnProdn/";
+  private baseUrl="http://172.16.16.52:8100/PostSpinning/api/YarnProdn/";
 
   constructor(private http:HttpClient) { }
 
