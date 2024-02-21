31479f9407857f837dfab4b0e6e9a955701ef4c4
Gowreesh E A <gowreeshea@gmail.com>
2024-02-21 13:20:57 +0530

    Removed Drag and Drop

--- src/app/component/pages/postspinning/postspinning.component.ts
+++ src/app/component/pages/postspinning/postspinning.component.ts
@@ -1,65 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
-import { TopcardsComponent } from './topcards/topcards.component';
-import { MachineutilComponent } from './machineutil/machineutil.component';
-import { CounteffComponent } from './counteff/counteff.component';
-import { CountprodnComponent } from './countprodn/countprodn.component';
-import { MachineprodnComponent } from './machineprodn/machineprodn.component';
-import { Rg1Component } from './rg1/rg1.component';
-import { VarietyprodnComponent } from './varietyprodn/varietyprodn.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-postspinning',
   templateUrl: './postspinning.component.html',
   styleUrls :['./postspinning.component.scss']
 })
-export class PostspinningComponent implements OnInit {
-  items = [
-    { component: TopcardsComponent, name: 'Top Cards'},
-    { component: Rg1Component, name: 'RG1 Stocks', size:'col-xl-6 col-lg-6 col-sm-12 box-col-4' },
-    { component: VarietyprodnComponent, name: 'Variety Production', size:'col-xl-6 col-lg-6 col-sm-12 box-col-4' },
-    { component: CountprodnComponent, name: 'Count Wise Production', size:'col-xl-6 col-lg-6 col-sm-12 box-col-4' },
-    { component: CounteffComponent, name: 'Count Wise Efficiency %', size:'col-xl-6 col-lg-6 col-sm-12 box-col-4'},
-    { component: MachineprodnComponent, name: 'Autoconer Machine Wise Production', size:'col-xl-12 col-lg-12 col-sm-12 box-col-4' },
-    { component: MachineutilComponent, name: 'Machine Wise Utilisation %', size:'col-xl-12 col-lg-12 col-sm-12 box-col-4' }
-  ];
+export class PostspinningComponent {
 
-  currentIndexs: number[];
-
-  ngOnInit(): void {
-    this.loadItemsOrder();
-    this.reorderItems();
-  }
-
-  loadItemsOrder(): void {
-    const storedOrder = localStorage.getItem('currentIndexs');
-    if (storedOrder) {
-      this.currentIndexs = JSON.parse(storedOrder);
-    } else {
-      // If currentIndexs not found in localStorage, initialize with default order
-      this.currentIndexs = this.items.map((_, index) => index);
-      localStorage.setItem('currentIndexs', JSON.stringify(this.currentIndexs));
-    }
-  }
-
-  reorderItems(): void {
-    const reorderedItems = this.currentIndexs.map(index => this.items[index]);
-    this.items = reorderedItems;
-  }
-
-  drop(event: CdkDragDrop<any[]>): void {
-    const currentIndexItem = this.currentIndexs[event.currentIndex];
-    const prevIndexItem = this.currentIndexs[event.previousIndex];
-    this.currentIndexs[event.currentIndex] = prevIndexItem;
-    this.currentIndexs[event.previousIndex] = currentIndexItem;
-    localStorage.setItem('currentIndexs', JSON.stringify(this.currentIndexs));
-
-    const movedItem = this.items[event.previousIndex];
-    this.items.splice(event.previousIndex, 1);
-    this.items.splice(event.currentIndex, 0, movedItem);
-  }
-
-  getGridClasses(size: string | undefined): string {
-    return size || '';
-  }
 }
--- src/app/shared/component/header/dashmenu/dashmenu.component.ts
+++ src/app/shared/component/header/dashmenu/dashmenu.component.ts
@@ -1,9 +1,30 @@
-import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-dashmenu",
   templateUrl: "./dashmenu.component.html",
   styleUrl: "./dashmenu.component.scss",
 })
-export class DashmenuComponent {}
+export class DashmenuComponent implements OnInit {
+
+
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  navigateToRoute(routePath: string): void {
+    const currentRoute = this.router.url;
+    if (currentRoute === routePath) {
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false; // This forces the route to reload
+      this.router.navigate([routePath], { relativeTo: this.route });
+    } else {
+      this.router.navigate([routePath]);
+    }
+
+  }
+
+
+}
