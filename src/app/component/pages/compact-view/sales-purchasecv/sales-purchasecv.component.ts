5de923cc6cbc59715d2315af261a0dfbe6b96b64
Gowreesh E A <gowreeshea@gmail.com>
2024-02-28 14:39:45 +0530

    cards modified

--- src/app/component/pages/compact-view/sales-purchasecv/sales-purchasecv.component.ts
+++ src/app/component/pages/compact-view/sales-purchasecv/sales-purchasecv.component.ts
@@ -1,13 +1,16 @@
+// sales-purchase.component.ts
 import { Component } from "@angular/core";
 import { DateService } from "src/app/services/date.service";
 import { RmiDashboardService } from "src/app/services/rmi-dashboard.service";
 
+
+
 @Component({
-  selector: 'app-sales-purchasecv',
-  templateUrl: './sales-purchasecv.component.html',
-  styleUrl: './sales-purchasecv.component.scss'
+  selector: "app-sales-purchasecv",
+  templateUrl: "./sales-purchasecv.component.html",
+  styleUrls: ["./sales-purchasecv.component.scss"],
 })
-export class SalesPurchasecvComponent {
+export class SalesPurchasecvComponent  {
   topCardDetails: any;
 
 commonData:any[];
@@ -30,67 +33,24 @@ divCode:string='01';
       this.fromDate=this.calendar.formattedfromDate;
       this.lotYear=this.calendar.lotYear;
       this.rmiService.getTopCardDetails(this.divCode, this.yearStart, this.yearEnd,this. fromDate,this. toDate, this.lotYear).subscribe((data) => {
-        console.log(data); // Log the received data
+        console.log(data); 
         this.preparedData(data);
         this.loadData=false
       });
     })
   }
 
- 
 
   preparedData(data:any):void{
-this.topCardDetails=data[0]
-    this.commonData = [
-
-    {
-        icon: 'new-order',
-        num:this.topCardDetails.noOfVariety,
-        title: 'No. of variety',
-        color: 'secondary'
-    },
-   
-    {
-      symbol: 'icofont icofont-scroll-bubble-right',
-      style: 'font-size: 25px; display: inline-block; border-radius: 50%; padding: 30px; color: rgb(80, 202, 196);',
-      num: this.topCardDetails.openingStkBales+'/'+this.topCardDetails.openingStkValue, 
-      title: 'Opening Stock',
-      color: 'primary'
-  },
-   {
-    symbol: 'icon-receipt',
-    style: 'font-size: 24px; font-weight: lighter; display: inline-block; border-radius: 50%; padding: 30px; color: rgb(232, 175, 76);',
-    num: this.topCardDetails.reciptBales+'/'+this.topCardDetails.reciptValue,
-    title: 'Receipt',
-    color: 'warning'
-  }, 
-  {
-    icon: 'rate',
-    num: this.topCardDetails.sales+'/'+this.topCardDetails.reciptReturn,
-   title: 'SR-Return',
-     color: 'success'
-  },
-   {
-    icon: 'issue-icon',  
-    showAlertTriangle: true, 
-    num: this.topCardDetails.issueBales+'/'+this.topCardDetails.issueValue,
-    title: 'Issue',
-    color: 'secondary'
-  },
-   {
-    icon: 'return-box',
-    num: this.topCardDetails.issueReturnBales+'/'+this.topCardDetails.issueReturnValue,
-    title: 'Issue Return',
-    color: 'primary'
-  },
-  {
-     symbol: 'icofont icofont-scroll-bubble-left',
-     style: 'font-size: 25px; display: inline-block; border-radius: 50%; padding: 30px; color: rgb(232, 175, 76);',
-    num: this.topCardDetails.closingStkBales+'/'+this.topCardDetails.closingStkValue,
-    title: ' Closing Stock',
-    color: 'warning'
-  },
-    
-  ]
+    this.topCardDetails=data[0]
+    this.commonData = [   
+   { description: this.topCardDetails.noOfVariety},
+    { description:this.topCardDetails.openingStkBales+'/'+this.topCardDetails.openingStkValue},
+    {  description:  this.topCardDetails.reciptBales+'/'+this.topCardDetails.reciptValue},
+    {description: this.topCardDetails.sales+'/'+this.topCardDetails.reciptReturn },
+    { description:this.topCardDetails.issueBales+'/'+this.topCardDetails.issueValue },
+    {  description: this.topCardDetails.issueReturnBales+'/'+this.topCardDetails.issueReturnValue },
+    { description:  this.topCardDetails.closingStkBales+'/'+this.topCardDetails.closingStkValue },
+  ];
   
 }}
